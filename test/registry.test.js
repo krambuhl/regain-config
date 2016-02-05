3130e8da30f40c11e0e7ec2616f426e3a9c7e64c
Evan Krambuhl <thekrambuhl@gmail.com>
2016-02-05 00:55:29 -0800

    better defaults

--- test/registry.test.js
+++ test/registry.test.js
@@ -1,11 +1,8 @@
-var Config = require('../dist').default;
-var Registry = require('../dist').Registry;
-
 var test = require('tape');
 
+var Config = require('../dist');
+var Registry = require('../dist/registry');
 
-var component1 = { type: 'tag', tagName: 'br' };
-function filter1(data) { return data; }
 function helper1(tree, props) { return tree; }
 function helper2(tree, props) { return tree; }
 
@@ -72,50 +69,4 @@ test('registry.unregister(name)', function(t) {
   reg.unregister('H2');
   t.equal(reg.get('H1'), helper1);
   t.equal(reg.get('H2') === undefined, true);
-});
-
-
-test('new Config()', function (t) {
-  var config = new Config();
-
-  t.plan(4);
-  t.equal(config instanceof Config, true);
-  t.equal(config.components instanceof Registry, true);
-  t.equal(config.helpers instanceof Registry, true);
-  t.equal(config.filters instanceof Registry, true);
-});
-
-test('config.get()', function (t) {
-  var config = new Config();
-  var parts = config.get();
-
-  t.plan(3);
-  t.equal(Object.keys(parts.components).length, 0);
-  t.equal(Object.keys(parts.helpers).length, 0);
-  t.equal(Object.keys(parts.filters).length, 0);
-});
-
-test('config.get(type)', function (t) {
-  var config = new Config({
-    components: { C1: component1 },
-    helpers: { Helper1: helper1 },
-    filters: { f1: filter1 }
-  });
-
-  t.plan(2);
-  t.equal(config.get('components').C1, component1);
-  t.equal(config.get('helpers').Helper1, helper1);
-});
-
-test('new Config({ components, helpers, filters })', function (t) {
-  var config = new Config({
-    components: { C1: component1 },
-    helpers: { Helper1: helper1 },
-    filters: { f1: filter1 }
-  });
-
-  t.plan(3);
-  t.equal(config.components.get().C1, component1);
-  t.equal(config.helpers.get().Helper1, helper1);
-  t.equal(config.filters.get().f1, filter1);
 });
