1a181620d5bf2ece59af2f83aac718bbe3c5a6c0
Evan Krambuhl <thekrambuhl@gmail.com>
2016-03-20 20:08:15 -0700

    generic

--- test/config.test.js
+++ test/config.test.js
@@ -4,6 +4,7 @@ var Config = require('../dist');
 var Registry = require('rogain-registry');
 
 var component1 = { type: 'tag', tagName: 'br' };
+function transform1(data) { return data; }
 function filter1(data) { return data; }
 
 
@@ -13,17 +14,36 @@ test('new Config()', function (t) {
   t.plan(3);
   t.equal(config instanceof Config, true);
   t.equal(config.components instanceof Registry, true);
-  t.equal(config.filters instanceof Registry, true);
+  t.equal(config.transforms instanceof Registry, true);
 });
 
-test('new Config({ components, filters })', function (t) {
+test('new Config({ components, transforms })', function (t) {
   var config = new Config({
     components: { C1: component1 },
-    filters: { f1: filter1 }
+    transforms: { t1: transform1 }
   });
 
   t.plan(2);
   t.equal(config.components.get().C1, component1);
+  t.equal(config.transforms.get().t1, transform1);
+});
+
+
+test('new Config({ }, [])', function (t) {
+  var config = new Config({
+    components: { C1: component1 },
+    transforms: { t1: transform1 }
+  }, [
+    'components',
+    'transforms',
+    'filters'
+  ]);
+
+  config.filters.register('f1', filter1);
+
+  t.plan(3);
+  t.equal(config.components.get().C1, component1);
+  t.equal(config.transforms.get().t1, transform1);
   t.equal(config.filters.get().f1, filter1);
 });
 
@@ -33,40 +53,16 @@ test('config.get()', function (t) {
 
   t.plan(2);
   t.equal(Object.keys(parts.components).length, 0);
-  t.equal(Object.keys(parts.filters).length, 0);
+  t.equal(Object.keys(parts.transforms).length, 0);
 });
 
 test('config.get(type)', function (t) {
   var config = new Config({
     components: { C1: component1 },
-    filters: { f1: filter1 }
+    transforms: { t1: transform1 }
   });
 
   t.plan(2);
   t.equal(config.get('components').C1, component1);
-  t.equal(config.get('filters').f1, filter1);
-});
-
-test('registerComponent() / unregisterComponent', function (t) {
-  var config = new Config();
-  
-  t.plan(2);
-
-  config.registerComponent('C1', component1);
-  t.equal(config.get('components').C1, component1);
-  
-  config.unregisterComponent('C1');
-  t.equal(config.get('components').C1 === undefined, true);
-});
-
-test('registerFilter() / unregisterFilter()', function (t) {
-  var config = new Config();
-
-  t.plan(2);
-  
-  config.registerFilter('F1', filter1);
-  t.equal(config.get('filters').F1, filter1);
-  
-  config.unregisterFilter('F1');
-  t.equal(config.get('filters').F1 === undefined, true);
+  t.equal(config.get('transforms').t1, transform1);
 });
